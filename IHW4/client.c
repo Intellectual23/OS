00c7b9a1939faf15feaf058a17925c5e44c74f99
egor4k <egorerror00@mail.ru>
2024-06-20 06:08:42 +0300

    code for 4-5

---
+++ IHW4/client.c
@@ -0,0 +1,77 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
+
+#define PORT 8080
+#define SERVER_IP "127.0.0.1"
+#define M 3
+#define N 4
+#define K 5
+
+typedef struct {
+    int id;
+    int line;
+    int bookCase;
+    int position;
+    int taken;
+    int checked;
+} Book;
+
+typedef struct {
+    int bookId;
+    int taken;
+} Message;
+
+int allChecked = 0;
+
+void student_work(int client_socket, struct sockaddr_in *address, socklen_t len) {
+    Book book_to_check;
+    Message msg;
+
+    recvfrom(client_socket, &book_to_check, sizeof(Book), 0, (struct sockaddr *) &address, &len);
+
+    if (book_to_check.taken == 1) {
+        msg.taken = 1;
+    } else {
+        msg.taken = 0;
+    }
+
+    msg.bookId = book_to_check.id;
+
+    sendto(client_socket, &msg, sizeof(msg), 0, (struct sockaddr *) &address, len);
+    usleep(1000000);
+}
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    socklen_t len = sizeof(serv_addr);
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("Socket creation error");
+        return -1;
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+
+
+    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        return -1;
+    }
+
+    printf("- CLIENT CONNECTED TO SERVER");
+    while (1) {
+        student_work(sock, &serv_addr, len);
+    }
+
+    close(sock);
+
+    return 0;
+}
---
+++ IHW4/server.c
@@ -0,0 +1,136 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#define PORT 8080
+#define M 3
+#define N 4
+#define K 5
+
+typedef struct {
+    int id;
+    int line;
+    int bookCase;
+    int position;
+    int taken;
+    int checked;
+} Book;
+
+typedef struct {
+    int bookId;
+    int taken;
+} Message;
+
+Book library[M * N * K];
+int AllChecked = 0; // status for library if all books are checked
+
+void PrintBookStatus(int bookId) {
+    Book book = library[bookId];
+    printf("Book #%d is at line %d, bookcase %d, position %d. It is %s.\n", book.id, book.line, book.bookCase,
+           book.position, (book.taken ? "taken" : "not taken"));
+}
+
+void PrintCheckedStatus(int bookId) {
+    printf("Book #%d has already been checked!\n", bookId);
+}
+
+void CheckLibStatus() {
+    for (int i = 0; i < M * N * K; ++i) {
+        if (library[i].checked == 0) {
+            return;
+        }
+    }
+    AllChecked = 1;
+}
+
+// Генерация книг в библиотеке
+void GenerateLibrary() {
+    srand(time(0));
+    int id = 0;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            for (int k = 0; k < 5; ++k) {
+                library[id].id = id;
+                library[id].line = i;
+                library[id].bookCase = j;
+                library[id].position = k;
+                library[id].checked = 0;
+                library[id].taken = (rand() % 2 == 0);
+                ++id;
+            }
+        }
+    }
+}
+
+void *HandleClient(void *arg) {
+    pthread_mutex_t book_mutex = PTHREAD_MUTEX_INITIALIZER;
+    int sock_fd = *(int *) arg;
+    struct sockaddr_in cl;
+    socklen_t len = sizeof(cl);
+    Message msg;
+    while (AllChecked == 0) {
+        srand(time(NULL));
+        int randomIndex = rand() % (M * N * K);
+        Book book_to_check = library[randomIndex];
+        sendto(sock_fd, &book_to_check, sizeof(Book), 0, (struct sockaddr *) &cl, len);
+        if ((recvfrom(sock_fd, &msg, sizeof(msg), 0, (struct sockaddr *) &cl, &len) > 0)) {
+            //pthread_mutex_lock(&book_mutex);
+            Book checked_book = library[msg.bookId];
+            int checked_book_status = msg.taken;
+            library[checked_book.id].taken = checked_book_status;
+            if (library[checked_book.id].checked == 1) {
+                PrintCheckedStatus(checked_book.id);
+            } else {
+                PrintBookStatus(checked_book.id);
+                library[checked_book.id].checked = 1;
+            }
+            //pthread_mutex_unlock(&book_mutex);
+        }
+        CheckLibStatus();
+    }
+    close(sock_fd);
+    return NULL;
+}
+
+int main() {
+    int server_fd;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) == 0) {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("Setsockopt failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+    GenerateLibrary();
+    printf("LIBRARY IS GENERATED!\n");
+    while (AllChecked == 0) {
+        printf("- NEW CONNECTION IS ACCEPTED");
+        pthread_t thread;
+        pthread_create(&thread, NULL, HandleClient, &server_fd);
+        pthread_join(thread, NULL);
+
+        close(server_fd);
+    }
+    return 0;
+}
